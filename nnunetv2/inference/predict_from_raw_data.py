b355e3a6654b3f42c38c32c1717f73b1b0be5cd4
Ricardo Brioso <ricardo.brioso@polimi.it>
2024-03-19 17:36:44 +0100

    1

--- nnunetv2/inference/predict_from_raw_data.py
+++ nnunetv2/inference/predict_from_raw_data.py
@@ -105,8 +105,7 @@ class nnUNetPredictor(object):
             configuration_manager.network_arch_init_kwargs,
             configuration_manager.network_arch_init_kwargs_req_import,
             num_input_channels,
-            plans_manager.get_label_manager(dataset_json).num_segmentation_heads,
-            enable_deep_supervision=False
+            plans_manager.get_label_manager(dataset_json).num_segmentation_heads
         )
 
         network = medcam.inject(network, label=1, replace=True, backend="gcam", layer='seg_outputs.5')
